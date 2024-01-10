2eb255918dfdec96996bb2b42af1d7024a79b82c
Jú Garcia <spacejugarcia@gmail.com>
2024-01-10 12:18:51 -0300

    update

---
+++ todo/src/components/Todo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Todo = ({todo}) => {
+  return (
+    <div className="todo">
+    <div className="content">
+      <p>{todo.text}</p>
+      <p className="category"> ({todo.category})</p>
+    </div>
+    <div>
+      <button>Feito</button>
+      <button>Não feito</button>
+    </div>
+  </div>
+  )
+}
+
+export default Todo;
